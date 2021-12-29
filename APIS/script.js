04514ce819c921facaddfb5f334e60c729099fad
Zairo Bastos <zairo_vianahd@hotmail.com>
2021-12-29 02:40:15 -0300

    Exercicio de API finalizado

---
+++ APIS/script.js
@@ -0,0 +1,29 @@
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+
+const imageCat = document.querySelector('.cat');
+const alterar = document.querySelector('.Alterar');
+
+const getCats = async ()=>{
+    try {
+        const data = await fetch(BASE_URL);
+        const json = await data.json();
+        let dados={
+            id: json.id,
+            url: json.url,
+            webpurl: json.webpurl,
+            x: json.x,
+            y: json.y
+        }
+        return dados;
+    } catch (error) {
+        console.log(error);
+    }
+}
+const loadImg = async () => {
+    gatos= await getCats();
+    imageCat.src = gatos.webpurl;
+}
+
+alterar.addEventListener('click',loadImg);
+
+loadImg();
